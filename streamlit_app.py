ebd3b7bdb6fe1ac1b3b7853fbe061ed944068ed3
greg oehmen <greg.oehmen@gmail.com>
2025-02-14 16:13:44 -0800

    Update streamlit_app.py
     Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+# adding streamlit connect as per lesson 8 to move from SiS to SniS
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
