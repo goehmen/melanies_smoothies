7abd091578ef833f2049b11715a30218f5975aa7
greg oehmen <greg.oehmen@gmail.com>
2025-02-26 16:50:06 -0800

    Update streamlit_app.py
    fetching from array using loc/iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('Frui
 
 # Convert the snaowpark dataframe to a Pandas dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose Up To 5 Ingredients:'
@@ -38,6 +38,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         # Adding smoothiefruit api call support - new section to display smoothiefroot nutrituion information
         st.subheader(fruit_chosen + ' ' + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
